166832c51ac972827ccb963506a38777675ceed5
emiliabrgf <burggrafemilia@gmail.com>
2021-04-13 09:25:52 +0200

    added scenemanager scene 1 and wrong/right button scripts

--- Assets/Scripts/SceneController.cs
+++ Assets/Scripts/SceneController.cs
@@ -1,18 +1,26 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class SceneController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public Button wrongButton1;
+    public Button wrongButton2;
+    private string errorMsg = "Try again!";
+    public void ButtonRightAnswer()
     {
-        
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
 
-    // Update is called once per frame
-    void Update()
+    public void Button1WrongAnswer()
     {
-        
+        wrongButton1.GetComponentInChildren<Text>().text = errorMsg;
+    }
+
+    public void Button2WrongAnswer()
+    {
+
     }
 }
